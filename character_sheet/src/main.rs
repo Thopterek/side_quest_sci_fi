df3ce9ad43b4a3a5666aab6308bdc30f8d0cdb5a
Thopterek <nataniel.dziadzia@gmail.com>
2026-04-24 23:40:31 +0200

    A bit by a bit
    Working on towards understanding each section of the pdf and why when
    reading with help I am getting more confused

--- character_sheet/src/main.rs
+++ character_sheet/src/main.rs
@@ -1,4 +1,5 @@
-use pdf_writer::{Name, Pdf, Ref, TextStr, types::FieldType};
+use pdf_writer::types::{AnnotationFlags, BorderType, FieldType};
+use pdf_writer::{Finish, Name, Pdf, Rect, Ref, TextStr};
 
 fn main() -> std::io::Result<()> {
     let mut pdf = Pdf::new();
@@ -15,5 +16,17 @@ fn main() -> std::io::Result<()> {
     fields.field_type(FieldType::Text);
     fields.text_value(TextStr(""));
     fields.text_default_value(TextStr(""));
+
+    let mut rectange_for_fields = fields.into_annotation();
+    rectange_for_fields.rect(Rect::new(108.0, 730.0, 208.0, 748.0));
+    rectange_for_fields
+        .border_style()
+        .style(BorderType::Underline);
+    rectange_for_fields
+        .appearance_characteristics()
+        .border_color_rgb(0.24, 0.24, 0.24);
+    rectange_for_fields.flags(AnnotationFlags::PRINT);
+    rectange_for_fields.finish();
+
     Ok(())
 }
